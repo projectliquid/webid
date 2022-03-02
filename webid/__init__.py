626ba683e5ff47b65325efe270e80fbb5d359914
Kushal Das <mail@kushaldas.in>
2022-03-02 13:56:36 +0100

    Adds direct Graph usage
    We are now directly using Graph objects inside of our class.

--- webid/__init__.py
+++ webid/__init__.py
@@ -17,54 +17,42 @@ class WebID:
         document_kind: str = "ProfileDocument",
         kind: str = "Person",
         solid: bool = False,
+        oidcissuer: str = "",
     ) -> None:
         """Initiates a WebID class. The URL is the primary ID URL.
 
         :args document_kind: ProfileDocument
         :args kind: str, Person or Agent.
+        :args oidcissuer: URL of the OIDC provider. Default empty string.
         """
         self.url = url
         self.solid = solid
+        self.oidcissuer = oidcissuer
         if document_kind not in ["ProfileDocument"]:
             raise ValueError("Not correct document kind.j")
-        self.tripples = {}
 
         if kind not in ["Person", "Agent"]:
             raise ValueError("Wrong data type, can be either Person or Agent")
         self.me_url = f"{self.url}/#me"
-        internal = []
+        # This is our actual graph inside
+        self.g = Graph()
+        # The two default namespaces
+        self.g.bind("foaf", FOAF)
+        self.g.bind("solid", SOLID)
+        self.webid_uri = URIRef(self.url)
+        self.g.add((self.webid_uri, RDF.type, FOAF.PersonalProfileDocument))
+        # For #me URL of the card
+        self.me_uri = URIRef(self.me_url)
+        self.g.add((self.webid_uri, FOAF.maker, self.me_uri))
+        self.g.add((self.webid_uri, FOAF.primaryTopic, self.me_uri))
+        # Now type of the WebID document
         if kind == "Person":
-            internal.append((RDF.type, FOAF.Person))
+            self.g.add((self.me_uri, RDF.type, FOAF.Person))
         else:
-            internal.append((RDF.type, FOAF.Agent))
-        self.tripples[self.me_url] = internal
-
-    def add_oidcissuer(self, issuer: str) -> None:
-        """Adds the oidc issuer to the WebID document.
-
-        :args issuer: URL of the OIDC provider.
-        """
-        self.tripples["oidcissuer"] = issuer
+            self.g.add((self.me_uriRDF.type, FOAF.Agent))
+        # Add the OIDC issuer value if available
+        if self.oidcissuer:
+            self.g.add((self.me_uri, SOLID.oidcIssuer, URIRef(self.oidcissuer)))
 
     def serialize(self) -> str:
-        g = Graph()
-        g.bind("foaf", FOAF)
-        g.bind("solid", SOLID)
-        self_uri = URIRef(self.url)
-        g.add((self_uri, RDF.type, FOAF.PersonalProfileDocument))
-        internal = self.tripples.get(self.me_url, [])
-        me_uri = URIRef(self.me_url)
-        # Add maker
-        g.add((self_uri, FOAF.maker, me_uri))
-        # Add primary topic
-        g.add((self_uri, FOAF.primaryTopic, me_uri))
-
-        # Each entry is a tuple of predicate & object
-        for entry in internal:
-            g.add((me_uri, entry[0], entry[1]))
-
-        # Add any OIDC issuer
-        if "oidcissuer" in self.tripples:
-            g.add((me_uri, SOLID.oidcIssuer, URIRef(self.tripples["oidcissuer"])))
-
-        return g.serialize(format="turtle")
+        return self.g.serialize(format="turtle")
